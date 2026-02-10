06881e20f608f543d517ff62cb6241da7c10c4b0
heric <herickdan@hotmail.com>
2026-02-09 21:25:40 -0300

    remove unnecessary enum validation

--- backend/src/main/java/com/aceleradev/backend/commons/enums/Role.java
+++ backend/src/main/java/com/aceleradev/backend/commons/enums/Role.java
@@ -1,26 +1,7 @@
 package com.aceleradev.backend.commons.enums;
 
 public enum Role {
-    SELLER(1),
-    MANAGER(2),
-    INTERN(3);
-
-    private int code;
-
-    private Role(int code){
-        this.code = code;
-    }
-
-    public int getCode() {
-        return code;
-    }
-
-    public static Role valueOf(int code){
-        for (Role value : Role.values()){
-            if (value.getCode() == code){
-                return value;
-            }
-        }
-        throw new IllegalArgumentException("Invalid Role code");
-    }
+    SELLER,
+    MANAGER,
+    INTERN;
 }
