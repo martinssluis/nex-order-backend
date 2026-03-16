7892f13aa7d23716458fc61d509561c1e56d0f37
nicollaswendel <nicollaswendel1623@gmail.com>
2026-03-15 21:27:04 -0300

    refactor: update Role ENUM

--- src/main/java/com/aceleradev/nexorder/commons/enums/Role.java
+++ src/main/java/com/aceleradev/nexorder/commons/enums/Role.java
@@ -1,7 +1,18 @@
 package com.aceleradev.nexorder.commons.enums;
 
+import com.fasterxml.jackson.annotation.JsonValue;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public enum Role {
-    SELLER,
-    MANAGER,
-    INTERN;
+
+    SELLER("Vendedor"),
+    MANAGER("Gestor"),
+    INTERN("Estagiário");
+
+    @JsonValue
+    private final String name;
+
 }
