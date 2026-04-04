ddf8cae075f9205da8665be0e71e14ac1c9c0434
Claudia Beathriz O. Pacheco <claudiabopacheco@gmail.com>
2026-04-04 12:00:53 -0300

    fix: add h2 headers

--- src/main/java/com/aceleradev/nexorder/infra/security/SecurityConfig.java
+++ src/main/java/com/aceleradev/nexorder/infra/security/SecurityConfig.java
@@ -23,6 +23,9 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
                 .csrf(csrf -> csrf.disable())
+                .headers(headers -> headers
+                        .frameOptions(frame -> frame.disable())
+                )
                 .sessionManagement(session -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 )
